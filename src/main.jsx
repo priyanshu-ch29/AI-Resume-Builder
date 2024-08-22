43c409f9a88587818e823e19c54aa2784ec85bb0
priyanshu-ch29 <priyanshuchaudhary1000@gmail.com>
2024-08-22 16:42:57 +0530

    Project Setup

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import SignIn from "./auth/SignIn";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: "/auth/signIn",
+    element: <SignIn />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
